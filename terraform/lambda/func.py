df3d70f4582ed56885bec2468d0e5ba81c7408ef
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-06-30 23:52:38 -0400

    terraform main.tf
    updated the main.tf file to simply attache the correct IAM role to the Lambda function. Internal Service Error was being run into as I tried to create a new role and attach it to the Lambda function through Terraform

---
+++ terraform/lambda/func.py
@@ -0,0 +1,40 @@
+import json
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("cloud-resume-challenge")
+
+def lambda_handler(event, context):
+    try:
+        # Get the current view count from DynamoDB
+        response = table.get_item(Key={
+            "id": "1"
+        })
+        if 'Item' in response:
+            views = int(response["Item"]["views"])
+        else:
+            views = 0  # Default to 0 if the item doesn't exist
+        
+        # Increment the view count
+        views += 1
+        print("pushed with terraform!")
+        print(views)
+        
+        # Update the view count in DynamoDB
+        table.put_item(Item={
+            "id": "1",
+            "views": views
+        })
+        
+        # Return the updated view count
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"views": views})
+        }
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+# Archive the Lambda function code
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/func.py"
+  output_path = "${path.module}/lambda/func.zip"
+}
+
+# Define the Lambda function using the existing IAM role
+resource "aws_lambda_function" "myfunc" {
+  filename         = data.archive_file.zip_the_python_code.output_path
+  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
+  function_name    = "myfunc"
+  role             = "arn:aws:iam::631242286372:role/service-role/cloud-resume-views-role-bnt3oikr" # Existing IAM role ARN
+  handler          = "func.lambda_handler"
+  runtime          = "python3.12"
+}
+
+# Define the Lambda function URL
+resource "aws_lambda_function_url" "url1" {
+  function_name      = aws_lambda_function.myfunc.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+}
